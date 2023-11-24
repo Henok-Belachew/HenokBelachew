64749dcf6ff8287caab3296d81b589dd918401ef
Henok Belachew <henokbelachewstu@gmail.com>
2023-11-24 05:18:17 +0300

    completed configuring Tailwind and React!

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className='absolute right-0'>
+      Tailwind is working! and react too!
+    </div>
+  )
+}
+
+export default App
