26ed47ceebb16c43d51763b61299eef83edae17e
Henok Belachew <henokbelachewstu@gmail.com>
2024-04-27 14:37:08 +0300

    professional skill sets section completed for large screens

---
+++ src/Data/ProfessionalSkillsData.tsx
@@ -0,0 +1,50 @@
+import figma from "../assets/images/Skillsets/Figma.svg";
+import adobeXD from "../assets/images/Skillsets/Adobe XD.svg";
+import expressJs from "../assets/images/Skillsets/Express Js.svg";
+import javascript from "../assets/images/Skillsets/JavaScript.svg";
+import mongodb from "../assets/images/Skillsets/MongoDB.svg";
+import mysqlLogo from "../assets/images/Skillsets/MySQL Logo.svg";
+import nodejs from "../assets/images/Skillsets/Nodejs.svg";
+import reactLogo from "../assets/images/Skillsets/React.svg";
+import tailwindCss from "../assets/images/Skillsets/Tailwind CSS.svg";
+import typescript from "../assets/images/Skillsets/TypeScript.svg";
+import adobePhotoshop from "../assets/images/Skillsets/Adobe Photoshop.svg";
+import adobeillustrator from "../assets/images/Skillsets/Adobe Illustrator.svg"
+
+export const skillsetImages = {
+    all: [
+      figma,
+      expressJs,
+      javascript,
+      mongodb,
+      adobeillustrator,
+      mysqlLogo,
+      nodejs,
+      adobeXD,
+      reactLogo,
+      tailwindCss,
+      typescript,
+      adobePhotoshop
+    ],
+    ui_ux_design: [
+      figma,
+      adobeXD,
+      adobeillustrator,
+      adobePhotoshop
+    ],
+    web_development: [
+      expressJs,
+      javascript,
+      mongodb,
+      mysqlLogo,
+      nodejs,
+      reactLogo,
+      tailwindCss,
+      typescript
+    ]
+  };
+  
+
+  
+  
+  export default skillsetImages
--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,17 +1,24 @@
-import service1 from "../assets/images/icon-app.svg"
-import service2 from "../assets/images/icon-design.svg"
-import service3 from "../assets/images/icon-dev.svg"
-import service4 from "../assets/images/icon-dev.svg"
+
 
 import serviceData from "../Data/ServiceData"
 import ServiceItem from "./ServiceItem"
 
+import skillsetImages from "../Data/ProfessionalSkillsData"
+import { useState } from "react"
+
+
+
+
 function About({activeTab}: {activeTab: string}) {
+
+  const [filter, setFilter] = useState('All')
   return (
     <div className={`article about ${activeTab === 'About' ? 'active' : ''}`}>
          <header>
           <h2 className="h2 article-title">About me</h2>
         </header>
+
+        {/* About Me Text */}
 
         <section className="about-text">
           <p>
@@ -48,6 +55,64 @@ function About({activeTab}: {activeTab: string}) {
             ))}
 
             
+
+          </ul>
+
+        </section>
+
+
+        
+        {/* Profesional Skillsets */}
+        
+
+        <section className="professional-skills">
+
+          <h3 className="h3 professional-skills-title">Professional Skillsets</h3>
+
+          {/* Filter */}
+          <ul className="filter-list">
+
+            <li className="filter-item">
+              <button className={filter === 'All' ? 'active' : ''} onClick={() => setFilter('All')} >All</button>
+            </li>
+
+            <li className="filter-item">
+              <button className={filter === 'UI/UX' ? 'active' : ''} onClick={() => setFilter('UI/UX')}>UI/UX design</button>
+            </li>
+
+            
+
+            <li className="filter-item">
+              <button className={filter === 'Web' ? 'active' : ''} onClick={() => setFilter('Web')}>Web development</button>
+            </li>
+
+          </ul>
+
+          {/* Skills List  */}
+          <ul className="skills-list has-scrollbar">  
+
+           { filter === 'All' && skillsetImages.all.map((img) => (
+
+                <img className="skillbox h-[80px]"  src={img}></img>
+                ))
+           }  
+
+           { filter === 'UI/UX' && skillsetImages.ui_ux_design.map((img) => (
+
+                <img className="skillbox h-[80px]"  src={img}></img>
+                ))
+           }  
+
+           { filter === 'Web' && skillsetImages.web_development.map((img) => (
+
+                <img className="skillbox h-[80px]"  src={img}></img>
+                ))
+           }      
+
+            
+            
+          
+
 
           </ul>
 
