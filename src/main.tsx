dd06110c544914179e526663a549154873d00832
Henok Belachew <henokbelachewstu@gmail.com>
2024-04-28 00:48:00 +0300

    route bug fixed

--- src/main.tsx
+++ src/main.tsx
@@ -6,13 +6,12 @@ import "./styles.css"
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
-// const publicUrl = process.env.P
 
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  // basename={process.env.PUBLIC_URL}
-  <BrowserRouter basename={process.env.PUBLIC_URL}>
+  
+  <BrowserRouter >
   <React.StrictMode>
 
 
@@ -20,12 +19,6 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <Route  path='/HenokBelachew' element={<App />} />
     
     </Routes>
-      
-
-      
-
-    
-    
     
   </React.StrictMode>
   </BrowserRouter>,
