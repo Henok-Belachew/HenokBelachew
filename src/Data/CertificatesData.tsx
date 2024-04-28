408709e54d374e6a78bbac7fc7f7464f8707a721
Henok Belachew <henokbelachewstu@gmail.com>
2024-04-28 20:37:47 +0300

    certificate section content added

---
+++ src/Data/CertificatesData.tsx
@@ -0,0 +1,55 @@
+import git from "../assets/images/Certificates/git.png"
+import googleux from "../assets/images/Certificates/googleux.png"
+import alxafrica from "../assets/images/Certificates/alxafrica.png"
+import networking from "../assets/images/Certificates/networking.png"
+import freecodecamp from "../assets/images/Certificates/freecodecamp.png"
+
+
+const Certificates = [
+    {
+        id: 1,
+        name: 'ALX Software Engineering',
+        link: 'https://intranet.alxswe.com/certificates/3TYENrs8ep',
+        issuingOrg: "ALX Africa",
+        dateIssue: "Sep 2023",
+        img: alxafrica
+    },
+    {
+        id: 3,
+        name: 'Google UX Design Spcialization',
+        link: 'https://www.coursera.org/account/accomplishments/specialization/certificate/JCWRN8W6DAYF',
+        issuingOrg: "Coursera",
+        dateIssue: "Aug 2023",
+        img: googleux
+
+    },
+    {
+        id: 2,
+        name: 'Networking Basics',
+        link: 'https://www.credly.com/badges/8c86e60b-5634-44a4-bf3b-193bb4e51888/public_url',
+        issuingOrg: "Cisco Networking Academy",
+        dateIssue: "Sep 2023",
+        img: networking
+    },
+    
+    {
+        id: 4,
+        name: 'Responsive Web Design',
+        link: 'https://www.freecodecamp.org/certification/Henok-Belachew/responsive-web-design',
+        issuingOrg: "Coursera",
+        dateIssue: "Dec 2022",
+        img: freecodecamp
+    },
+    {
+        id: 5,
+        name: 'Git Training',
+        link: 'https://simpli-web.app.link/e/aclJKBIHxsb',
+        issuingOrg: "Simplilearn",
+        dateIssue: "Aug 2022",
+        img: git
+    }
+]
+
+
+export default Certificates
+
