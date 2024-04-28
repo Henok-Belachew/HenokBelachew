f52fdf59c1bc3fa7f79e82e8239bed19ccad0ae5
Henok Belachew <henokbelachewstu@gmail.com>
2024-04-28 20:37:01 +0300

    certificate section is complete and responsive

--- src/components/Certificates.tsx
+++ src/components/Certificates.tsx
@@ -1,5 +1,6 @@
 import { useEffect } from "react"
-
+import CertificatesData from "../Data/CertificatesData"
+import { RxExternalLink } from "react-icons/rx";
 function Certificates({activeTab}: {activeTab: string}) {
   useEffect(() => {
     
@@ -11,6 +12,43 @@ function Certificates({activeTab}: {activeTab: string}) {
           <header>
             <h2 className="h2 article-title">Certificates</h2>
           </header>
+
+
+          <div className="certificates gap-[20px] justify-between flex flex-wrap">
+
+            {
+              CertificatesData.map((data) => (
+                <div className="w-[400px] mx-auto lg:mx-0 text-white-2">
+                  <img className="w-[400px] bg-cover h-[265px] " src={data.img} alt="" />
+
+                  <div className="mt-3">
+                    <h1>{data.name}</h1>
+                    <p className="font-[200] text-white-2 opacity-80">{data.issuingOrg}</p>
+
+                    <span className="flex  items-center justify-between">
+                      <p className="font-[200]">Issued {data.dateIssue}</p>
+
+                      <span onClick={() => window.open(data.link, '_blank')} className="flex gap-2 items-center opacity-70 hover:opacity-100 hover:text-orange-yellow-crayola hover:border-orange-yellow-crayola border px-8 py-1 rounded-lg cursor-pointer">
+                        
+                        <span >Verify</span>
+
+                        <RxExternalLink />
+
+                      </span>
+                      
+                    </span>
+
+                  </div>
+                  
+                  
+
+
+                </div>
+                
+              ))
+            }
+
+          </div>
         
       </div>
     )
